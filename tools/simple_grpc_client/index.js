1357c65ce0abc985368c0a84742709743873445a
Cuong Duy Nguyen <103cuong@gmail.com>
2020-09-21 09:17:30 +0700

    chore: stuff

--- tools/simple_grpc_client/index.js
+++ tools/simple_grpc_client/index.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const Mali = require('mali')
+const { logger } = require('juno-js')
 
 const PROTO_PATH = path.resolve(__dirname, './greeter.proto')
 
@@ -11,6 +12,7 @@ function main () {
     const app = new Mali(PROTO_PATH)
     app.use({ sayHello })
     app.start('127.0.0.1:50001')
+    logger.info('server ready at 0.0.0.0:50001')
 }
 
 main()
